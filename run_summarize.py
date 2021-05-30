b07448198f592b8449f2b03254d39a45abbc6b46
tuanbmhust <tuanbm.hust@gmail.com>
2021-05-31 02:37:56 +0700

    update summarized

--- run_summarize.py
+++ run_summarize.py
@@ -1,14 +1,17 @@
 from summarizer import Summarizer
 
-DATA_DIR = './data/plaintext/Chinh tri'
-SUMMARY_DIR = './data/bert_summary/Chinh tri'
+DATA_DIR = './data/plaintext/Xa Hoi'
+SUMMARY_DIR = './data/bert_summary/Xa Hoi'
 
-for i in range(1, 32):
+model = Summarizer()
+
+for i in range(1, 36):
     if i < 10:
-        filename = DATA_DIR + "/" + "CT0" + str(i) + ".txt"
+        filename = DATA_DIR + "/" + "XH0" + str(i) + ".txt"
     else:
-        filename = DATA_DIR + "/" + "CT" + str(i) + ".txt"
-    
+        filename = DATA_DIR + "/" + "XH" + str(i) + ".txt"
+    print(filename)
+
     title = ""
     paragraph = ""
     
@@ -16,14 +19,14 @@ for i in range(1, 32):
         title = f.readline()
         for line in f:
             paragraph += line.strip()
-
-    model = Summarizer()
+    
     result = ''.join(model(body=paragraph, ratio=0.4, min_length=30))
+    result = result.replace('_', ' ')
 
     if i < 10:
-        filename_output = SUMMARY_DIR + "/" + "CT0" + str(i) + ".txt"
+        filename_output = SUMMARY_DIR + "/" + "XH0" + str(i) + ".txt"
     else:
-        filename_output = SUMMARY_DIR + "/" + "CT" + str(i) + ".txt"
+        filename_output = SUMMARY_DIR + "/" + "XH" + str(i) + ".txt"
 
 
     with open(filename_output, "w", encoding='utf-8') as f:
